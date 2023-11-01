34a50536c74085bd9b998102bf1a17761a51415c
William <williamdmoore2002@gmail.com>
2023-11-01 16:10:34 +0000

    Update william_addOrder.py

--- Functions/william_addOrder.py
+++ Functions/william_addOrder.py
@@ -2,13 +2,16 @@ import psycopg2, getpass, pandas as pd
 from psycopg2 import Error
 try:
     connection = psycopg2.connect(
-        host="localhost", user = "BUILDER",
+        host="147.252.250.51", user = "C21437002",
         password=getpass.getpass(),
-        port="54321", database="postgres")
+        port="5432", database="postgres")
 
     cursor = connection.cursor()
+    
+    email = input("Enter account email: ")
+    pcname = input("Enter name of pc: ")
 
-    cursor.execute("CALL add_sale('F11111', '1', '2')")
+    cursor.execute("CALL addOrder(email,pcname)")
 
     connection.commit()
 
