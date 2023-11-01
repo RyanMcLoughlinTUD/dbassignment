691aef1976f0b03d631b7381dbb2ccaa5cf78e66
William <91558272+Willt-olive@users.noreply.github.com>
2023-11-01 15:48:41 +0000

    add error

--- Functions/William_addOrder.sql
+++ Functions/William_addOrder.sql
@@ -6,15 +6,25 @@ select * from cust_pc;
 create or replace function addOrder(
 buyer_email_f varchar(100),
 pc_name_f varchar(100)
-) returns void as $$
+)
+returns void
+language plpgsql
+as $$
 declare
 pc_id_f integer;
 buyer_id_f integer;
 begin
 	select buyer_id into buyer_id_f from cust_buyer where buyer_email_f = buyer_email;
+	if not found then
+		raise exception 'The email % does not exist', buyer_email_f;
+	end if;
 	select pc_id into pc_id_f from cust_pc where pc_name_f = pc_name;
 
 	insert into cust_order (pc_id, buyer_id, order_date, order_email)
 	values (pc_id_f, buyer_id_f, now(), buyer_email_f);
 end;
 $$
+
+
+select count(*) from cust_buyer where buyer_email = 'dd';
+
