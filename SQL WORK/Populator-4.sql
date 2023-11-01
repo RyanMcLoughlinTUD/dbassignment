eddb6a2dd8feb464a87d07e5880cde31636ad1ac
William <91558272+Willt-olive@users.noreply.github.com>
2023-11-01 15:39:57 +0000

    Update Populator-4.sql
    email incorrect

--- SQL WORK/Populator-4.sql
+++ SQL WORK/Populator-4.sql
@@ -1,7 +1,7 @@
 insert into cust_buyer (buyer_name, buyer_address, buyer_email)
-values ('William', 'Lissadell, Piedmont, Riverstown, Dundalk, Co.Louth', 'C21437002@mytudublin.ie,');
+values ('William', 'Lissadell, Piedmont, Riverstown, Dundalk, Co.Louth', 'c21437002@mytudublin.ie,');
 insert into cust_buyer (buyer_name, buyer_address, buyer_email)
-values ('Paul', '208, DaTrees, Dublin 4', 'C21359216@mytudublin.ie,');
+values ('Paul', '208, DaTrees, Dublin 4', 'c21359216@mytudublin.ie,');
 insert into cust_buyer (buyer_name, buyer_address, buyer_email, buyer_phone)
 values ('Ryan', 'Hikeasle, Kanhdahar, Afghanistan, 0H10712', 'c21431604@mytudublin.ie,', '087-420-6969');
 insert into cust_buyer (buyer_name, buyer_address, buyer_email)
@@ -12,7 +12,7 @@ values (1, 1, '01/11/2023', 'c21437002@mytudublin.ie');
 insert into cust_order (pc_id, buyer_id, order_date, order_email)
 values (5, 1, '16/08/2023', 'c21437002@mytudublin.ie');
 insert into cust_order (pc_id, buyer_id, order_date, order_email)
-values (8, 2, '26/03/2023', 'C21359216@mytudublin.ie');
+values (8, 2, '26/03/2023', 'c21359216@mytudublin.ie');
 insert into cust_order (pc_id, buyer_id, order_date, order_email)
 values (9, 3, '05/06/2023', 'c21431604@mytudublin.ie');
 insert into cust_order (pc_id, buyer_id, order_date, order_email)
