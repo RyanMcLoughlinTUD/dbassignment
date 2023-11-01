e5bede4a81d3c40f1ca8c36c3ef3d9ec88153f43
William <williamdmoore2002@gmail.com>
2023-11-01 16:03:48 +0000

    Create william_addOrder.py

---
+++ Functions/william_addOrder.py
@@ -0,0 +1,16 @@
+import psycopg2, getpass, pandas as pd
+from psycopg2 import Error
+try:
+    connection = psycopg2.connect(
+        host="localhost", user = "BUILDER",
+        password=getpass.getpass(),
+        port="54321", database="postgres")
+
+    cursor = connection.cursor()
+
+    cursor.execute("CALL add_sale('F11111', '1', '2')")
+
+    connection.commit()
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL:", error)
