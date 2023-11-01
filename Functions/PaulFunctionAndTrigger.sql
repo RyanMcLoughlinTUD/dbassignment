fc416006a614f652a0b346c07a9149edffe42935
Paul Murnane <113908234+PaulMcoding@users.noreply.github.com>
2023-11-01 16:45:16 +0000

    Update PaulFunctionAndTrigger.sql

--- Functions/PaulFunctionAndTrigger.sql
+++ Functions/PaulFunctionAndTrigger.sql
@@ -21,20 +21,23 @@ begin
 	select count(*) into v_phone_check from cust_supplierdetails where supplier_phone = f_supplierphone;
 	if v_phone_check > 0 then
 		raise exception 'This phone number is already linked to another business';
+		rollback;
 	end if;
 	insert into cust_supplierdetails (supplier_name, supplier_address, supplier_phone) 
 	values (f_suppliername, f_supplieraddress, f_supplierphone)
 	returning supplier_id into v_supplier_id;
 	return v_supplier_id;
+	commit;
 exception
 when others then
 	raise info 'Error name:%',sqlerrm;
 	raise info 'Error state:%',sqlstate;
+	rollback;
 	return null;
 end;
 $$;
 
-select addSupplierDetails('Wingus.com', '108 The Bog, Bogtown USA', '088-202-1010');
+select addSupplierDetails('wappus.com', '1082 er 2, Cheepsn USA', '131-123-1253');
 
 select * from supplier_log;
 select * from cust_supplierdetails;
